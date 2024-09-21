982af6575c956d9903f93e7f918a01ead220c6db
shubham sharma <shubhamsharma01504@gmail.com>
2024-09-21 17:04:30 +0530

    Initial Commit

---
+++ CoreDataPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataPracticeApp
+//
+//  Created by shubham sharma on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
